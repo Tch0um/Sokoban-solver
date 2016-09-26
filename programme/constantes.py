d95a686c00c2fbc8eab7f46eb3178ce7e1f232f2
QuentinDEME <21507097@etu-unicaen.fr>
2016-09-26 14:12:42 +0200

    Ajout d'élément

--- programme/constantes.py
+++ programme/constantes.py
@@ -5,6 +5,22 @@ from pygame.locals import *
 pygame.init()
 
 #Image de l'icone
-image_icone = "images/caisse.png"
+c_image_icone = "images/caisse.png"
+
 #Taille de la fénêtre
-taille_fenetre = 720,480
+c_taille_fenetre = 720,480
+
+#fichier du niveau
+c_fichier_niveau="levels/niveau.txt"
+
+#test temporaire
+image_accueil = "imgs/accueil.png"
+image_fond = "imgs/fond.jpg"
+image_mur = "images/caisse30.png"
+image_depart = "imgs/depart.png"
+image_arrivee = "imgs/arrivee.png"
+
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
--- programme/sokoban.py
+++ programme/sokoban.py
@@ -2,12 +2,13 @@ import pygame
 from pygame.locals import *
 
 from constantes import *
+from classes import *
 pygame.init()
 
 #Création de la fenêtre
-fenetre = pygame.display.set_mode(taille_fenetre)
+fenetre = pygame.display.set_mode(c_taille_fenetre)
 #Icone
-icone = pygame.image.load(image_icone).convert_alpha()
+icone = pygame.image.load(c_image_icone).convert_alpha()
 pygame.display.set_icon(icone)
 #Titre
 pygame.display.set_caption('Sokoban')
@@ -24,10 +25,15 @@ pygame.display.set_caption('Sokoban')
 #BOUCLE INFINI
 continuer = 1
 while continuer:
+    pygame.time.Clock().tick(30) #limitation "fps" pour pas avoir le cpu à fond
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             continuer = 0
 
+    niveau = Niveau(c_fichier_niveau)
+    niveau.generer()
+    niveau.afficher(fenetre)
+    
     pygame.display.flip()
 
 pygame.quit()
